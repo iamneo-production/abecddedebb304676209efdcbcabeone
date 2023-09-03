2671cc5424dbba37b7380badd7120aba17c1b9a6
Iamneo <techadmin@iamneo.ai>
2023-09-03 16:03:00 +0000

    Backup Commit

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ const express = require('express');
 const app = express();
 const port = 8081;
 
-//app.use('/static', express.static('public'))
+app.use('/static', express.static('public'))
 app.use(express.static('public'))
 app.listen(port, () => console.log(`listening on port ${port}!`));
